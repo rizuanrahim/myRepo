99ba6bfef7c1ae61742ca3fc76fa2d5cfe070000
Rizuan <rizuan.rahim@gmail.com>
2013-12-03 11:27:32 +0800

    2nd commit

--- src/main/java/training/pictag/domain/UploadPicture.java
+++ src/main/java/training/pictag/domain/UploadPicture.java
@@ -6,10 +6,22 @@
 
 package training.pictag.domain;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+
 /**
  *
  * @author User
  */
 public class UploadPicture {
+    private String id;
+    private Date captureDate;
+    private User owner;
+    private String imageLocation;
+    private  BigDecimal latitude;
+    private  BigDecimal longitude;
+    private String gpsCoordinate;
+    private String remarks;
     
 }
